671d1935176a2e23989762e4647d77d29373ccd0
Inon Bercu <inonbr@gmail.com>
2020-01-08 10:50:31 +0200

    more python recap

---
+++ python_fundamentals/controll_flow.py
@@ -0,0 +1,9 @@
+def bigger(x):
+  if (x > 10):
+    print("bigger then 10")
+  elif (x > 5):
+    print("bigger then 5")
+  else:
+    print("you are too small!")
+
+bigger(11)
---
+++ python_fundamentals/loops.py
@@ -0,0 +1,16 @@
+def while_loop():
+  i = 0
+
+  while (i <= 10):
+    print(i)
+    i += 1
+
+def for_loop():
+  for x in range(10, 21):
+    print(x)
+
+def word_loop():
+  name = "inon Bercu"
+
+  for c in name:
+    print(c)
